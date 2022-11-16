42ae578387c833779320cefb1162c6aa939093c3
2305 <npjt2305@yahoo.com>
2022-11-16 10:18:17 -0500

    ExerciseCompleted

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -1,10 +1,19 @@
 ﻿namespace StaticExercise
 {
-    public class Program
+    public class Program //Do I need to put this to static?
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var celcius = TempConverter.FarenheitToCelcius(68); //20
+
+            var farenheit = TempConverter.CelciusToFarenheit(20); //68
+
+            Console.WriteLine("After Conversion:");
+            Console.WriteLine($"celcius {celcius}");
+            Console.WriteLine($"farenheit {farenheit}");
+                                                                
+
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+
+        public static double FarenheitToCelcius(double farenheit)
+
+        { 
+            var result = (farenheit - 32) / 1.8;
+
+            return result;
+        } 
+    
+        public static double CelciusToFarenheit(double celcius)
+    {
+        return (celcius * 9) / 5 + 32;
+    }
+}
